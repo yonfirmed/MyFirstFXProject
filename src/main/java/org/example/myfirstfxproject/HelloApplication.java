cbbbf35a6874ac4615a4fc409c840df0f38c85f7
Yon <136676602+yonfirmed@users.noreply.github.com>
2024-12-12 20:07:42 +0100

    Update HelloApplication.java

--- src/main/java/org/example/myfirstfxproject/HelloApplication.java
+++ src/main/java/org/example/myfirstfxproject/HelloApplication.java
@@ -16,7 +16,9 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-public static void test(){}
+public static void test(){
+    test();
+}
     public static void main(String[] args) {
         launch();
     }
